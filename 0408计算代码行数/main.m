b2848fc1162ceff50ed1ba43e8ad21b476eebc20
Apple <apple@hp-2.local>
2015-04-08 16:47:13 +0800

    Initial Commit

---
+++ 0408计算代码行数/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  0408计算代码行数
+//
+//  Created by Apple on 15-4-8.
+//  Copyright (c) 2015年 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
